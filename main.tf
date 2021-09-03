3020375459f9ac50a9ad48e3316e528c4f05f27d
bleons36 <89231748+bleons36@users.noreply.github.com>
2021-09-03 11:33:26 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,6 +22,21 @@ resource "docker_container" "nginx" {
     external = 80
   }
 }
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
 
 resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
