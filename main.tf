787db15f8416bdce2cf2417c2930b441311a9767
bleons36 <89231748+bleons36@users.noreply.github.com>
2021-09-03 11:35:17 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,13 @@ terraform {
   required_providers {
     docker = {
       source = "kreuzwerker/docker"
+      version = "2.15.0"
     }
   }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
 }
 
 resource "docker_image" "nginx" {
